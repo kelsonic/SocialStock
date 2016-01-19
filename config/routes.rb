540e47139382b48d8d1134b4be1596dce2d8ca4c
kgadams93 <kgadams93@gmail.com>
2016-01-19 17:58:34 +0000

    Add my friends page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   
 end
