2ecd283b3d408473e70815698a630e247502a9ed
kgadams93 <kgadams93@gmail.com>
2016-01-19 16:13:22 +0000

    Add first and last name to user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   
 end
