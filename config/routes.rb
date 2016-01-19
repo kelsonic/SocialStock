1ee68f61eae60323ec197c9fd9523b6464c02b5d
kgadams93 <kgadams93@gmail.com>
2016-01-19 18:21:43 +0000

    Add friend search view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
   
   devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
